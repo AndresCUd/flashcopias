934b480f5dbe0bc6911d8b89ab554426c5eddb32
AndresCUd <43498268+AndresCUd@users.noreply.github.com>
2020-07-08 13:30:10 -0500

    update
    boostrap

--- new_web/flashcopias/src/App.vue
+++ new_web/flashcopias/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <navbar />
     <router-view />
   </div>
 </template>
+<script>
+import navbar from "./components/navbar";
+export default {
+  components: {
+    navbar,
+  },
+};
+</script>
 
 <style>
 #app {
---
+++ new_web/flashcopias/src/components/navbar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" variant="info">
+      <b-navbar-brand href="#" class="col-lg-3">
+        <img
+          src="../assets/logo.png"
+          class="d-inline-block align-left"
+          alt="Kitten"
+        />
+      </b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown text="Impresiones" right>
+            <b-dropdown-item href="#">B&N - Color</b-dropdown-item>
+            <b-dropdown-item href="#">CD's</b-dropdown-item>
+            <b-dropdown-item href="#">Ploter</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-dd-divider class="mx-4" inset vertical></b-dd-divider>
+          <b-nav-item-dropdown text="Empastes" right>
+            <b-dropdown-item href="#">Fino</b-dropdown-item>
+            <b-dropdown-item href="#">Rusticoss</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-dd-divider class="mx-4" inset vertical></b-dd-divider>
+          <b-nav-item-dropdown text="Escaner" right>
+            <b-dropdown-item href="#">Documentos</b-dropdown-item>
+            <b-dropdown-item href="#">Gran Formato</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-dd-divider class="mx-4" inset vertical></b-dd-divider>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+<style>
+nav {
+  background-image: url("../assets/fondo_top.png");
+  margin: 0%;
+  padding: 0%;
+}
+span {
+  font-size: 3vmin;
+  font-weight: bold;
+}
+a {
+  font-size: 3vmin;
+  font-weight: bold;
+}
+nav-brand {
+  width: 55%;
+  margin: 0%;
+  padding: 0%;
+}
+img {
+  width: 100%;
+}
+</style>
